df657b28d9bcc63730a1dae57fb8ff51d4d6eedc
eugene <eugene.tarus21@gmail.com>
2024-01-15 12:21:38 +0300

    still working

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -1,12 +1,31 @@
 package com.chess.engine.board;
 
 import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class Tile {
 
-    int tileCoordinate;
+    protected final int tileCoordinate;
 
-    Tile(int tileCoordinate){
+    private static final Map<Integer,emptyTile> Empty_Tile_Cache = createAllPossibleEmptyTiles();
+
+    private static Map<Integer,emptyTile>  createAllPossibleEmptyTiles(){
+        final Map<Integer,emptyTile> emptyTileMap = new HashMap<>();
+
+        for(int i = 0;i<64;i++){
+            emptyTileMap.put(i,new emptyTile(i));
+        }
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+
+    public static Tile createTile(final int tileCoordinates, final Piece piece){
+        return piece != null ? new occupiedTile(tileCoordinates, piece ): Empty_Tile_Cache.get(tileCoordinates);
+    }
+
+    private Tile(int tileCoordinate){
         this.tileCoordinate = tileCoordinate;
 
     }
@@ -15,7 +34,7 @@ public abstract class Tile {
 
     public static final class emptyTile extends Tile{
 
-        emptyTile(int coordinate){
+        private emptyTile(final int coordinate){
             super(coordinate);
         }
         @Override
@@ -29,9 +48,9 @@ public abstract class Tile {
 
     }
     public static final class occupiedTile extends Tile {
-        Piece pieceOnTile;
+        private final  Piece pieceOnTile;
 
-        occupiedTile(int tileCoordinate, Piece pieceOnTile){
+        private occupiedTile(int tileCoordinate, Piece pieceOnTile){
             super(tileCoordinate);
             this.pieceOnTile = pieceOnTile;
         }
--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,4 +1,13 @@
 package com.chess.engine.pieces;
 
 public class Piece {
+    protected final int piecePosition;
+    protected final Alliance pieceAlliance;
+
+    Piece(final int  piecePosition, final Alliance pieceAlliance){
+        this.pieceAlliance = pieceAlliance;
+        this.piecePosition = piecePosition;
+    }
+
+
 }
